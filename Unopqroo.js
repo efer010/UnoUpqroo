28973bb1dc459a31f55bf40b6a62bbe992d69f91
efer010 <eferjadiel10@gmail.com>
2023-10-09 19:45:19 -0500

    commit inicial
    Juego de uno

---
+++ Unopqroo.js
@@ -0,0 +1,43 @@
+const rl = require ("readline-sync");
+// juego uno 
+var deck = [];
+var colors = ["yellow","blue", "red", "green"];
+var types = ["comun", "especial", "comodin"];
+
+var card = {
+    color: null, // color puede ser amarillo, rojo, azul y verde
+    number: null, // 0-9
+    type: null //comun, especiales y comodines
+};
+
+for (var cColor = 0; cColor < colors.length; cColor++ ) {
+   
+    for (var cNumber = 0; cNumber < 10; cNumber++ ) {
+        deck.push({color: colors[cColor], number: cNumber, type: "comun"})
+    }
+    for (var cNumber1 = 1; cNumber1 < 10; cNumber1++ ) {
+        deck.push({color: colors[cColor], number: cNumber1, type: "comun"})
+    }
+ deck.push({color: colors[cColor], number: "+2", type: "especiales"});
+ deck.push({color: colors[cColor], number: "+2", type: "especiales"});
+ deck.push({color: colors[cColor], number: "B", type: "especiales"});
+ deck.push({color: colors[cColor], number: "B", type: "especiales"});
+ deck.push({color: colors[cColor], number: "R", type: "especiales"});
+ deck.push({color: colors[cColor], number: "R", type: "especiales"});
+}
+
+ deck.push({color: null, number: "+4", type: "Comodin"});
+ deck.push({color: null, number: "+4", type: "Comodin"});
+ deck.push({color: null, number: "+4", type: "Comodin"});
+ deck.push({color: null, number: "+4", type: "Comodin"});
+ deck.push({color: null, number: "CC", type: "Comodin"});
+ deck.push({color: null, number: "CC", type: "Comodin"});
+ deck.push({color: null, number: "CC", type: "Comodin"});
+ deck.push({color: null, number: "CC", type: "Comodin"});
+
+//console.log(deck);
+
+const players = rl.question("Cuantos jugadores seran?", [])
+
+
+console.log(players);
